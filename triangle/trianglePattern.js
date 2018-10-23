e9c0e0d72d0c8502bb15a62f5fcba2f8dfe0a422
Swagata Chakraborty <cswagata@IN-swagata.local>
2018-10-23 12:01:04 +0530

    Add triangle

---
+++ triangle/trianglePattern.js
@@ -0,0 +1,39 @@
+const repeatStrings = function(numberOfRepeatition, string) {
+  let repeatedStrings = "";
+  for(let charCount=1; charCount<=numberOfRepeatition; charCount++) {
+    repeatedStrings += string;
+  }
+  return repeatedStrings;
+}
+
+const generateRow = function(triangleType, rowLength, height) {
+  let prefix, suffix;
+  if(triangleType == "left") {
+    prefix = repeatStrings(rowLength,"*");
+    suffix = repeatStrings(height - rowLength," ");
+  }
+  if(triangleType == "right") {
+    prefix = repeatStrings(height - rowLength," ");
+    suffix = repeatStrings(rowLength,"*");
+  }
+  return prefix + suffix;
+}
+
+const generateTriangle = function(height, triangleType) {
+  let delimeter = "";
+  let triangle = "";
+  for(let rowNumber=1; rowNumber <= height; rowNumber++) {
+    let row = generateRow(triangleType, rowNumber, height);
+    triangle += delimeter + row;
+    delimeter = "\n";
+  }
+  return triangle;
+}
+
+const main = function(){
+  let triangleType = process.argv[2];
+  let triangleHeight = +process.argv[3];
+  console.log(generateTriangle(triangleHeight, triangleType));
+}
+ 
+main();
