3eb07b458a232f1a5006908223b61751ad145866
Swagata Chakraborty <cswagata@IN-swagata.local>
2018-10-24 17:58:11 +0530

    introduced app tests

---
+++ app_tests.sh
@@ -0,0 +1,3 @@
+./run_test.sh generateDiamond.js appTestData/allDiamondInput appTestData/allDiamondOutput
+./run_test.sh generateTriangle.js appTestData/allTriangleInput appTestData/allTriangleOutput
+./run_test.sh generateRectangle.js appTestData/allRectangleInput appTestData/allRectangleOutput
